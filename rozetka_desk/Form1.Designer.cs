3d7b21aa89b763f57374fa498fa491f63dae0ba6
kekaan <iskandier_shakirov@mail.ru>
2021-07-22 02:35:09 +0300

    Version 1

---
+++ rozetka_desk/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace rozetka_desk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.richTextBox_sensor_readings = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(719, 348);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // richTextBox_sensor_readings
+            // 
+            this.richTextBox_sensor_readings.Location = new System.Drawing.Point(756, 12);
+            this.richTextBox_sensor_readings.Name = "richTextBox_sensor_readings";
+            this.richTextBox_sensor_readings.Size = new System.Drawing.Size(88, 319);
+            this.richTextBox_sensor_readings.TabIndex = 1;
+            this.richTextBox_sensor_readings.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(756, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 374);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox_sensor_readings);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "ver_1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RichTextBox richTextBox_sensor_readings;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ rozetka_desk/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rozetka_desk
+{
+    public partial class Form1 : Form
+    {
+        UdpClient Client = new UdpClient(8081);
+        const string ip = "127.0.0.1";
+        const int port = 8081;
+        int seconds = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Client.BeginReceive(new AsyncCallback(Recieve), null);
+            }
+            catch(Exception ex)
+            {
+                richTextBox_sensor_readings.Text += ex.Message.ToString();
+            }
+        }
+
+        void Recieve(IAsyncResult res)
+        {
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+
+            while (true)
+            {
+                var buffer = new byte[256];
+                var size = 0;
+                var data = new StringBuilder();
+                EndPoint senderendPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                do
+                {
+                    size = udpSocket.ReceiveFrom(buffer, ref senderendPoint);
+                    data.Append(Encoding.UTF8.GetString(buffer));
+                }
+                while (udpSocket.Available > 0);
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    richTextBox_sensor_readings.Text += data;
+                    richTextBox_sensor_readings.Text += "\r\n";
+                    this.chart1.Series[0].Points.AddXY(seconds, float.Parse(data.ToString(), CultureInfo.InvariantCulture.NumberFormat));
+                }));
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+        }
+    }
+}
